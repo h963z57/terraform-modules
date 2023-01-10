ddbeddacc5e1d5213ae31544a3699daadb4fea4b
h963z57 <111640710+h963z57@users.noreply.github.com>
2023-01-10 04:49:17 -0500

    Update main.tf

--- yc_network/main.tf
+++ yc_network/main.tf
@@ -14,19 +14,19 @@ provider "yandex" {
 #=================== Create public network=================================
 
 resource "yandex_vpc_network" "public" {
-  name = "${var.env}-public-${var.network_name}"
+  name = "${var.env}-network-${var.network_name}"
 }
 
 resource "yandex_vpc_subnet" "public" {
   count          = length(var.public_subnet_cidrs)
-  name           = "${var.env}-public-${count.index}"
+  name           = "${var.env}-subnet-${count.index}"
   zone           = var.zone
   network_id     = yandex_vpc_network.public.id
   v4_cidr_blocks = [element(var.public_subnet_cidrs, count.index)]
 }
 
 #=================== Create private network=================================
-
+/*
 resource "yandex_vpc_network" "private" {
   name = "${var.env}-private-${var.network_name}"
 }
@@ -38,3 +38,4 @@ resource "yandex_vpc_subnet" "private" {
   network_id     = yandex_vpc_network.private.id
   v4_cidr_blocks = [element(var.private_subnet_cidrs, count.index)]
 }
+*/
