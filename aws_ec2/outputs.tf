fe3a83ff3d90e4ed858d05d11b2d7e293608495e
h963z57 <contact@h963z57.com>
2023-12-20 15:24:26 +0500

    Add outputs to aws_ec2

--- aws_ec2/outputs.tf
+++ aws_ec2/outputs.tf
@@ -1,3 +1,11 @@
 output "ec2_id" {
   value = aws_instance.module.id
 }
+
+output "ec2_public_ip" {
+  value = aws_instance.module.public_ip
+}
+
+output "ec2_private_ip" {
+  value = aws_instance.module.private_ip
+}
