f31cdb7470ae58d85ceec4eb86bccb2186fc8e13
Roman Bykov <contact@h963z57.com>
2025-03-22 13:31:05 +0300

    [5.0.0] - 22-Mar-2025

--- aws_ec2/outputs.tf
+++ aws_ec2/outputs.tf
@@ -3,6 +3,7 @@ output "ec2_instances" {
     for instance in aws_instance.module :
     instance.id => {
       ec2_id       = instance.id
+      name         = replace(instance.tags["Name"], "${var.env}-", "")
       private_ipv4 = instance.private_ip
       public_ipv4  = instance.public_ip
       public_ipv6  = lookup(instance, "ipv6_addresses", null)
